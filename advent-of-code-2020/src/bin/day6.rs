0e24c026deb91f0f2ec88ec722e5fb12acf16cef
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2020-12-07 11:02:54 -0800

    Finish day 6

--- advent-of-code-2020/src/bin/day6.rs
+++ advent-of-code-2020/src/bin/day6.rs
@@ -2,10 +2,17 @@ use advent_of_code_2020::day6;
 use std::fs;
 
 fn main() {
-    let customs_records =
-        day6::parse_records(&fs::read_to_string("day6-input.dat").expect("file input error"));
+    let input = fs::read_to_string("day6-input.dat").expect("file input error");
+    let customs_records = day6::parse_union_records(&input);
 
     let sum_counts: usize = customs_records.iter().map(|record| record.len()).sum();
 
     println!("sum of counts {}", sum_counts);
+
+    let intersection_records = day6::parse_intersection_records(&input);
+
+    let sum_intersection_counts: usize =
+        intersection_records.iter().map(|record| record.len()).sum();
+
+    println!("sum of counts {}", sum_intersection_counts);
 }
--- advent-of-code-2020/src/day6.rs
+++ advent-of-code-2020/src/day6.rs
@@ -1,8 +1,22 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
-pub fn parse_records(input: &str) -> Vec<HashSet<char>> {
+pub fn parse_union_records(input: &str) -> Vec<HashSet<char>> {
     input
         .split("\n\n")
         .map(|record| record.chars().filter(|c| c.is_ascii_alphabetic()).collect())
+        .collect()
+}
+
+pub fn parse_intersection_records(input: &str) -> Vec<HashSet<char>> {
+    input
+        .split("\n\n")
+        .map(|record| {
+            record
+                .lines()
+                .map(|line| line.chars().collect::<HashSet<char>>())
+                .fold1(|acc, set| acc.intersection(&set).copied().collect())
+                .expect("there's an empty record")
+        })
         .collect()
 }
